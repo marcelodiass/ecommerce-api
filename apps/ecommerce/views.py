5a2a7d284187ae019c06c152f0403a7bd342e189
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-16 23:35:19 -0300

    viewsets

--- apps/ecommerce/views.py
+++ apps/ecommerce/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from apps.ecommerce.models import Cliente, Vendedor, Produto, Compra
+from apps.ecommerce.serializers import ClienteSerializer, VendedorSerializer, CompraClienteSerializer, CompraSerializer, ProdutoSerializer
+from rest_framework import viewsets, generics
 
-# Create your views here.
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+    
+
+class VendedorViewSet(viewsets.ModelViewSet):
+    queryset = Vendedor.objects.all()
+    serializer_class = VendedorSerializer
+
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+
+class CompraViewSet(viewsets.ModelViewSet):
+    queryset = Compra.objects.all()
+    serializer_class = CompraSerializer
+    
+
+class CompraClienteList(generics.APIView):
+    def get_queryset(self):
+        queryset = Compra.objects.filter(cliente_id=self.kwargs['pk'])
+        return queryset
+
+    serializer_class = CompraClienteSerializer
+    
+    
